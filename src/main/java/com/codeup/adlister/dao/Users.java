a4f6fabe2550428347ddc29dbf71ac9241bdf273
Johann Williams <johann.williams01@gmail.com>
2023-07-31 10:21:58 -0500

    added delete method to Users.java and MySQLUsersDao.java. added comments. updated update methods to take in a user along with a string.

--- src/main/java/com/codeup/adlister/dao/Users.java
+++ src/main/java/com/codeup/adlister/dao/Users.java
@@ -7,7 +7,8 @@ import java.util.List;
 public interface Users {
     User findByUsername(String username);
     Long insert(User user);
-    void updatePassword(User user);
-    void updateEmail(User user);
-    void updateUsername(String username, User user);
+    void updatePassword(User user, String password);
+    void updateEmail(User user, String email);
+    void updateUsername(User user, String username);
+    void deleteUser(User user);
 }
